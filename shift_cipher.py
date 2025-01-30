8eed65fadafb4a860d29ad41a57735e4678d1a45
HwestCode <hwest@haverford.edu>
2025-01-29 22:04:25 -0500

    Update shift_cipher.py

--- shift_cipher.py
+++ shift_cipher.py
@@ -15,6 +15,13 @@ def bruteForce(cipher: str):
     for i in range(26):
         print('For shift of ' + str((i + 25) % 26) + ' characters, code said \"' + shifter(cipher, (i + 25) % 26) + "\"")
 
-# bruteForce("mbfx yebxl ebdx tg tkkhp")
+def main():
+    while True:
+        inp = input('Enter a lowercase string to shift (can include spaces), followed by a comma and the number'
+                    ' by which to shift. Or, enter 0 to quit: ')
+        if inp == "0":
+            break
+        else:
+            print(shifter(inp.split(',')[0], int(inp.split(',')[1])))
 
-print(shifter("modular arithmetic and spreadsheets", 10))
+main()
