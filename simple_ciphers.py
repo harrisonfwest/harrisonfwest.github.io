0acd8577694795f2f92f4f7696ca13f9d4933919
HwestCode <hwest@haverford.edu>
2025-02-03 21:50:39 -0500

    Update simple_ciphers.py

---
+++ simple_ciphers.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+def shifter(cipher: str, shift: int):
+    text = list(cipher)
+    ct = []
+    for t in text:
+        if t == " ":
+            ct.append(" ")
+        else:
+            ct.append(chr(((ord(t) - 96) + shift) % 26 + 96))
+    return ''.join(ct)
+
+
+def bruteForce(cipher: str):
+    for i in np.arange(1, 26, 1):
+        print(f'For {i} shift, plaintext said \"{shifter(cipher, abs(i-26))}\". Decrypt by shifting an additional {abs(i-26)}')
+
+def main():
+    while True:
+        inp = input('Enter a lowercase string to shift (can include spaces), followed by a comma and the number'
+                    ' by which to shift. Or, enter 0 to quit: ')
+        if inp == "0":
+            break
+        else:
+            print(shifter(inp.split(',')[0], int(inp.split(',')[1])))
+
+
+def multCipher(cipher: str, shift: int):
+    text = list(cipher)
+    ct = []
+    for t in text:
+        if t == " ":
+            ct.append(" ")
+        else:
+            ct.append(chr((((ord(t) - 96) * shift) % 26) + 96))
+    return ''.join(ct)
+
+print()
