962871267473658b4f78f29ae5c77fc7737bfa50
HwestCode <hwest@haverford.edu>
2025-02-03 22:14:36 -0500

    Update simple_ciphers.py

--- simple_ciphers.py
+++ simple_ciphers.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def shifter(cipher: str, shift: int):
+def addCipher(cipher: str, shift: int) -> str:
     text = list(cipher)
     ct = []
     for t in text:
@@ -11,21 +11,11 @@ def shifter(cipher: str, shift: int):
     return ''.join(ct)
 
 
-def bruteForce(cipher: str):
+def bruteForce(cipher: str) -> None:
     for i in np.arange(1, 26, 1):
-        print(f'For {i} shift, plaintext said \"{shifter(cipher, abs(i-26))}\". Decrypt by shifting an additional {abs(i-26)}')
+        print(f'For {i} shift, plaintext said \"{addCipher(cipher, abs(i-26))}\". Decrypt by shifting an additional {abs(i-26)}')
 
-def main():
-    while True:
-        inp = input('Enter a lowercase string to shift (can include spaces), followed by a comma and the number'
-                    ' by which to shift. Or, enter 0 to quit: ')
-        if inp == "0":
-            break
-        else:
-            print(shifter(inp.split(',')[0], int(inp.split(',')[1])))
-
-
-def multCipher(cipher: str, shift: int):
+def multCipher(cipher: str, shift: int) -> str:
     text = list(cipher)
     ct = []
     for t in text:
@@ -36,10 +26,61 @@ def multCipher(cipher: str, shift: int):
     return ''.join(ct)
 
 def affineCipher(cipher: str, multShift: int, addShift: int):
-    return (shifter(multCipher(cipher, multShift), addShift))
+    return (addCipher(multCipher(cipher, multShift), addShift))
+
+def keywordCipher(cipher: str, keyword: str, keyLetter : str) -> str:
+    # TODO: FINISH THIS FUNCTION
+    codeDict = {
+        1 : '',
+        2 : '',
+        3 : '',
+        4 : '',
+        5: '',
+        6: '',
+        7: '',
+        8: '',
+        9: '',
+        10: '',
+        11: '',
+        12: '',
+        13: '',
+        14: '',
+        15: '',
+        16: '',
+        17: '',
+        18: '',
+        19: '',
+        20: '',
+        21: '',
+        22: '',
+        23: '',
+        24: '',
+        25: '',
+        26: '',
+    }
+    used = []
+    for t in keyword:
+        if t in used:
+            continue
+        else:
+            used.append(t)
+        # now we have no duplicates in the keyword
+
+    charNum = ord(t) - 96 # only subtract 96 because the dictionary is 1-indexed rather than 0-indexed as for mod math
+    newUsed = []
+    for t in used:
+        codeDict[charNum] = t
+        newUsed.append(t)
+        if charNum + 1 > 26:
+            charNum = charNum - 25
+        else:
+            charNum = charNum + 1
+    # used[] is array of keyword w/o duplicates; newUsed is array of letters already inserted into alphabet key
+    # charNum is the current
+
 
 pt = ('when the government violates the peoples rights insurrection is for the people and for each portion of'
       ' the people the most sacred of the rights and the most indispensable of duties')
-print(shifter(pt, 16))
+print(addCipher(pt, 16))
 print(multCipher(pt, 16))
 print(affineCipher(pt, 3, 24))
